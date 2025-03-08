5cef33dec1fcc3f1cd67b2a458c517725c5d4be4
Hilmansyah <Hilm4nsyah@gmail.com>
2025-03-08 22:43:37 +0700

    Update browser.cpp

--- browser/browser.cpp
+++ browser/browser.cpp
@@ -7,6 +7,7 @@ Browser::Browser(const Settings &settings) : settings_(settings), jsInjector_(se
 CefLifeSpanHandler *Browser::GetLifeSpanHandler() { return this; }
 CefLoadHandler *Browser::GetLoadHandler() { return this; }
 CefRenderProcessHandler *Browser::GetRenderProcessHandler() { return this; }
+CefResourceRequestHandler *Browser::GetResourceRequestHandler() { return this; }
 
 void Browser::OnAfterCreated(CefRefPtr<CefBrowser> browser) {
   CefBrowserSettings browserSettings;
@@ -23,4 +24,11 @@ void Browser::OnAfterCreated(CefRefPtr<CefBrowser> browser) {
 void Browser::OnContextCreated(CefRefPtr<CefBrowser> browser, CefRefPtr<CefFrame> frame,
                               CefRefPtr<CefV8Context> context) {
   jsInjector_.inject(frame);
+}
+
+CefRefPtr<CefResourceHandler> Browser::GetResourceHandler(
+    CefRefPtr<CefBrowser> browser, CefRefPtr<CefFrame> frame,
+    CefRefPtr<CefRequest> request) {
+  // Implementasikan penanganan sumber daya (misalnya, blokir permintaan)
+  return nullptr;
 }
