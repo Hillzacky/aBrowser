e220409d4cf3626b20852581bbb7b89e1edb8e28
Hilmansyah <Hilm4nsyah@gmail.com>
2025-03-08 22:43:53 +0700

    Update browser.h

--- browser/browser.h
+++ browser/browser.h
@@ -3,23 +3,29 @@
 
 #include "../settings/settings.h"
 #include "js_injector.h"
+#include "res_handler.h"
 #include <include/cef_app.h>
 #include <include/cef_browser.h>
 
 class Browser : public CefClient,
                 public CefLifeSpanHandler,
                 public CefLoadHandler,
-                public CefRenderProcessHandler {
+                public CefRenderProcessHandler,
+                public CefResourceRequestHandler {
 public:
   Browser(const Settings &settings);
 
   CefLifeSpanHandler *GetLifeSpanHandler() override;
   CefLoadHandler *GetLoadHandler() override;
   CefRenderProcessHandler *GetRenderProcessHandler() override;
+  CefResourceRequestHandler *GetResourceRequestHandler() override;
 
   void OnAfterCreated(CefRefPtr<CefBrowser> browser) override;
   void OnContextCreated(CefRefPtr<CefBrowser> browser, CefRefPtr<CefFrame> frame,
                         CefRefPtr<CefV8Context> context) override;
+  CefRefPtr<CefResourceHandler> GetResourceHandler(
+      CefRefPtr<CefBrowser> browser, CefRefPtr<CefFrame> frame,
+      CefRefPtr<CefRequest> request) override;
 
 private:
   Settings settings_;
