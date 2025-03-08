c2f2ea8b89cad7190954033574c5b95ed86b840e
Hilmansyah <Hilm4nsyah@gmail.com>
2025-03-08 22:37:57 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,35 +1,31 @@
-#ifndef API_H
-#define API_H
 #include "ui/main_window.h"
 #include "settings/settings.h"
 #include <QtWidgets/QApplication>
 #include <include/cef_app.h>
-#include <string>
-
-class BrowserAPI {
-public:
-  static void loadURL(const std::string &url);
-  static void goBack();
-  static void goForward();
-  static void reload();
-  static void executeJavaScript(const std::string &code);
-  static std::string getHTML();
-  static std::string getURL();
-  static std::string getCookies();
-  static void setProxy(const std::string &proxy);
-};
-
-#endif
+#include <QDir>
+#include <QStandardPaths>
 
 int main(int argc, char *argv[]) {
   QApplication app(argc, argv);
   Settings settings;
-  CefMainArgs mainArgs(argc, argv);
+
+  // Atur direktori cache CEF
   CefSettings cefSettings;
+  QString cachePath = QStandardPaths::writableLocation(QStandardPaths::CacheLocation) + "/AntiDetectionBrowser";
+  QDir dir(cachePath);
+  if (!dir.exists()) {
+    dir.mkpath(".");
+  }
+  CefString(&cefSettings.cache_path) = cachePath.toStdString();
+
+  CefMainArgs mainArgs(argc, argv);
   CefInitialize(mainArgs, cefSettings, nullptr, nullptr);
+
   MainWindow mainWindow(settings);
   mainWindow.show();
+
   int result = app.exec();
+
   CefShutdown();
   return result;
 }
