3b5b4912380b46dd3ba04be2bafeef6e9e1829ff
Hilmansyah <Hilm4nsyah@gmail.com>
2025-03-08 22:00:17 +0700

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#ifndef API_H
+#define API_H
+
+#include <string>
+
+class BrowserAPI {
+public:
+  static void loadURL(const std::string &url);
+  static void goBack();
+  static void goForward();
+  static void reload();
+  static void executeJavaScript(const std::string &code);
+  static std::string getHTML();
+  static std::string getURL();
+  static std::string getCookies();
+  static void setProxy(const std::string &proxy);
+};
+
+#endif
