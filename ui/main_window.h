f4a027c49f57dcfe9a9cd710decf266ef7975704
Hilmansyah <Hilm4nsyah@gmail.com>
2025-03-08 22:36:54 +0700

    Update main_window.h

--- ui/main_window.h
+++ ui/main_window.h
@@ -6,6 +6,11 @@
 #include <QtWidgets/QMainWindow>
 #include <include/cef_app.h>
 #include <include/cef_browser.h>
+#include <QTabWidget>
+#include <QLineEdit>
+#include <QMap>
+#include <QMenu>
+#include <QToolBar>
 
 QT_BEGIN_NAMESPACE
 namespace Ui {
@@ -20,14 +25,43 @@ public:
   MainWindow(const Settings &settings, QWidget *parent = nullptr);
   ~MainWindow();
 
+public slots:
+  void loadURLFromAPI(const QString &url);
+  void goBackFromAPI();
+  void goForwardFromAPI();
+  void reloadFromAPI();
+  void executeJSFromAPI(const QString &code);
+  QString getHTMLFromAPI();
+  QString getURLFromAPI();
+  QString getCookiesFromAPI();
+  void setProxyFromAPI(const QString &proxy);
+  void setSettingsFromAPI(const QString &settingsJSON);
+  void takeScreenshotFromAPI(const QString &filePath);
+
 private slots:
   void on_actionNewTab_triggered();
-  // ... other slots
+  void on_actionCloseTab_triggered();
+  void on_actionExit_triggered();
+  void on_lineEditURL_returnPressed();
+  void on_actionBack_triggered();
+  void on_actionForward_triggered();
+  void on_actionReload_triggered();
 
 private:
   Ui::MainWindow *ui;
   Settings settings_;
-  // ... tab management
+  QTabWidget *tabWidget_;
+  QLineEdit *lineEditURL_;
+  QMap<QWidget *, CefRefPtr<CefBrowser>> browserMap_;
+  QMenu *fileMenu_;
+  QMenu *editMenu_;
+  QMenu *viewMenu_;
+  QToolBar *navigationToolbar_;
+
+  CefRefPtr<CefBrowser> getCurrentBrowser();
+  void addTab(CefRefPtr<CefBrowser> browser, const QString &title);
+  void removeTab(int index);
+  void setupUI();
 };
 
-#endif // MAIN_WINDOW_H
+#endif
